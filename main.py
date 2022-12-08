b5c17ee5c94b5af1cd0ab3843b7979216aed5954
Nathan Romanelli <nathan.romanelli@gmail.com>
2022-12-08 03:15:48 -0800

    Plotting, fixing stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import time
+import matplotlib.pyplot as plt
 
 def sum_squared_distance(a,b):
     return(sum((a[1:]-b[1:])**2))
@@ -38,7 +38,7 @@ def leave_one_out_accuracy(data,features,feature_to_add):
                     nn_label = object_to_compare[0]
         if nn_label == label:
             score+=1
-    return (float(precision(score*100,1000) / len(data)))
+    return (float(score / len(data)))
 
 def leave_one_out_accuracy_fast(data,features,feature_to_add):
     data_copy = data.copy()
@@ -61,8 +61,7 @@ def leave_one_out_accuracy_fast(data,features,feature_to_add):
         nn_label = data_copy[index_min,0]
         if nn_label == label:
             score+=1
-
-    return (float(score / len(data)))
+    return (float(score/len(data)))
 
 def feature_search_forward(data,epsilon):
     best_features = []
@@ -71,13 +70,18 @@ def feature_search_forward(data,epsilon):
     print("Starting Search \n")
     best_last_loop = 0
     default_rate = 0
+    features_all = []
+    results_all = []
+    features_all.append([])
+    features_all.append([])
 
     for i in data:
         if i[0] == 2:
             default_rate+=1
-    default_rate = precision(100*max(default_rate/len(data),1-default_rate/len(data)),100)
-    print(f"The default rate is {default_rate}%\n")
-    
+    default_rate = precision(max(default_rate/len(data),1-default_rate/len(data)),100)
+    print(f"The default rate is {default_rate}\n")
+    results_all.append(default_rate)
+
     for i in range(1,len(data[0])):
         feature_to_add = 0
         best_so_far_accuracy = 0
@@ -85,22 +89,23 @@ def feature_search_forward(data,epsilon):
         for k in range(1,len(data[0])):
             if k not in search_features:
                 accuracy = leave_one_out_accuracy_fast(data, search_features, k)
-                print(f"    Using feature(s) {search_features} and [{k}] accuracy is {accuracy}%")
+                print(f"    Using feature(s) {search_features} and [{k}] accuracy is {accuracy}")
 
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
                     feature_to_add = k
+        results_all.append(best_so_far_accuracy)
         if best_so_far_accuracy >= best_accuracy - epsilon:
             best_accuracy = best_so_far_accuracy
             best_features.append(feature_to_add)
         search_features.append(feature_to_add)
-        print("")
+        features_all.append(search_features.copy())
         if (best_so_far_accuracy < best_last_loop):
             print(f"Best accuracy decrease from {best_last_loop} to {best_so_far_accuracy}")
         best_last_loop = best_so_far_accuracy
         print(f"Feature {feature_to_add} was added to set of features\n")
 
-    return (best_features,best_accuracy)
+    return (best_features,best_accuracy,results_all,features_all)
 
 def feature_search_backward(data,epsilon):
     best_features = []
@@ -110,6 +115,9 @@ def feature_search_backward(data,epsilon):
         search_features.append(i)
     print("Starting Search \n")
     best_last_loop = 0
+    results_all = []
+    features_all = []
+    features_all.append(search_features.copy())
     
     for i in range(1,len(data[0])-1):
         feature_to_remove = 0
@@ -119,11 +127,13 @@ def feature_search_backward(data,epsilon):
             temp = search_features.copy()
             temp.remove(k)
             accuracy = leave_one_out_accuracy_fast(data, temp, np.inf)
-            print(f"    Using feature(s) {search_features} and removing [{k}] accuracy is {accuracy}%")
+            print(f"    Using feature(s) {search_features} and removing [{k}] accuracy is {accuracy}")
             if accuracy > best_so_far_accuracy:
                 best_so_far_accuracy = accuracy
                 feature_to_remove = k
+        results_all.append(best_so_far_accuracy)
         search_features.remove(feature_to_remove)
+        features_all.append(search_features.copy())
         if best_so_far_accuracy >= best_accuracy - epsilon:
             best_accuracy = best_so_far_accuracy
             best_features = search_features.copy()
@@ -137,10 +147,12 @@ def feature_search_backward(data,epsilon):
     for i in data:
         if i[0] == 2:
             default_rate+=1
-    default_rate = precision(100*max(default_rate/len(data),1-default_rate/len(data)),100)
-    print(f"The default rate is {default_rate}%\n")
+    default_rate = precision(max(default_rate/len(data),1-default_rate/len(data)),100)
+    print(f"The default rate is {default_rate}\n")
+    results_all.append(default_rate)
+    features_all.append([])
 
-    return (best_features,best_accuracy)
+    return (best_features,best_accuracy,results_all,features_all)
 
 print("What data set would you like to run?")
 print("    1) Small set 96")
@@ -182,7 +194,7 @@ time1 = time.time()
 if choice == "1":
     results = feature_search_forward(arr, 0)
 else:
-    results = feature_search_backward(arr, 0.01)
+    results = feature_search_backward(arr,0)
     pass
 time2 = time.time()
 time = time2 - time1
@@ -192,3 +204,13 @@ if time/60 < 1:
     print(f"Took {precision(time, 1000)} seconds to converge")
 else:
     print(f"Took {precision(time/60, 1000)} minutes to converge")
+
+accuracies = results[2]
+features = results[3]
+plt.plot(accuracies,'o-r',)
+plt.title('Accuracy of features for large set 85')
+plt.xlabel('Features removed')
+plt.ylabel('Accuracy')
+#plt.gca().set_xticklabels(features)
+plt.gca().set_yticklabels([f'{x:.0%}' for x in plt.gca().get_yticks()]) 
+plt.show()
